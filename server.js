1f423984bdc2e0aa2ec3ded8a7ef6fba827e7337
Drew Harry <drew.harry@gmail.com>
2011-09-16 19:15:26 -0400

    Added an identity function, plus some general cleanup.

--- server.js
+++ server.js
@@ -11,11 +11,21 @@ io.sockets.on('connection', function(socket) {
     
     // Sets up all the per-connection events that we need to think about.
     // For now, this is just a response to chat messages.   
-    socket.emit('chat.message', {message:"----Welcome to swarm!---------"});
+    socket.emit('chat.message', {message:"----Welcome to swarm!---------", from:"admin"});
     
     
     socket.on('chat.message', function(data) {
         // Mirror the message.
-        io.sockets.emit('chat.message', {message:data.message, from:socket.id});
-    })
+        io.sockets.emit('chat.message', {message:data.message, from:socket.name});
+    });
+    
+    socket.on('chat.identity', function(data) {
+        socket.name = data.name;
+        io.sockets.emit('chat.message', {message: socket.name + " has entered.", from:"admin"});
+    });
+    
+    socket.on('disconnect', function() {
+       io.sockets.emit('chat.message', {message:"Someone has left.", from:"admin"});
+    });
 });
+
