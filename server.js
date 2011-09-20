46fff23cad2e8fae2b49b7c5937f0d486f743dfe
Drew Harry <drew.harry@gmail.com>
2011-09-20 14:58:24 -0400

    connectedUsers list is now properly maintained on the server and enforced on identity queries.

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.get('/', function(req, res) {
 // TODO Do some sort of blocking on accepting connections until the redis
 // conneciton is actually alive.
 
+
+
 io.sockets.on('connection', function(socket) {
     
     
@@ -31,34 +33,35 @@ io.sockets.on('connection', function(socket) {
         
         
         // eventually check this against redis to see if the name is taken
-        
-        if(true) { 
-            socket.emit("identify", {state:"OK", username:data["username"]});
-            
-            socket.name = data.username;    
-            
-            socket.broadcast.emit('admin.message', {message: socket.name + " has entered.", from:"admin"});
-            
-            client.lrange("room.messages", -10, -1, function (err, res) {
-                console.log("lrange returned");
-                console.log(res);
-                for(msgIndex in res) {
-                    console.log(res[msgIndex]);
-                    msgObj = JSON.parse(res[msgIndex]);
-                    msgObj["past"] = true;
-                    socket.emit('chat.message', msgObj);
-                }
+        client.hexists("global:connectedUsers", data["username"], function (err, res) {
+            if(res == 0) { 
+                socket.emit("identify", {state:"OK", username:data["username"]});
+
+                socket.name = data.username;
                 
-                // Doing it here ensures that it appears after the past messages.
-                socket.emit('admin.message', {message: "You have joined the chat.", from:"admin"});
-            });
-            
-            
-        } else {
-            socket.emit("identify", {state:"TAKEN"})
-        }
-        
-    })
+                // Eventually, put a pointer to the user id in here, or something.
+                client.hset("global:connectedUsers", data["username"], true);
+
+                socket.broadcast.emit('admin.message', {message: socket.name + " has entered.", from:"admin"});
+
+                client.lrange("room.messages", -10, -1, function (err, res) {
+                    console.log("lrange returned");
+                    console.log(res);
+                    for(msgIndex in res) {
+                        console.log(res[msgIndex]);
+                        msgObj = JSON.parse(res[msgIndex]);
+                        msgObj["past"] = true;
+                        socket.emit('chat.message', msgObj);
+                    }
+
+                    // Doing it here ensures that it appears after the past messages.
+                    socket.emit('admin.message', {message: "You have joined the chat.", from:"admin"});
+                });
+            } else {
+                socket.emit("identify", {state:"TAKEN"})
+            }
+        });        
+    });
     
     socket.on('chat.message', function(data) {
         // Mirror the message.
@@ -71,37 +74,9 @@ io.sockets.on('connection', function(socket) {
         client.ltrim("room.messages", 0, 100);
     });
     
-    // socket.on('chat.identity', function(data) {
-    //     
-    //     // this will check to see if it's free or not, eventually)
-    //     if(true) 
-    //     
-    //     
-    //     
-    //     socket.name = data.name;
-    // 
-    //     socket.broadcast.emit('admin.message', {message: socket.name + " has entered.", from:"admin"});
-    //     
-    //     client.lrange("room.messages", -10, -1, function (err, res) {
-    //         console.log("lrange returned");
-    //         console.log(res);
-    //         for(msgIndex in res) {
-    //             console.log(res[msgIndex]);
-    //             msgObj = JSON.parse(res[msgIndex]);
-    //             msgObj["past"] = true;
-    //             socket.emit('chat.message', msgObj);
-    //         }
-    //         
-    //         // Doing it here ensures that it appears after the past messages.
-    //         socket.emit('admin.message', {message: "You have joined the chat.", from:"admin"});
-    //     });
-    //     
-    //     
-    //     // Send the last 10 messages.
-    // });
-    
     socket.on('disconnect', function() {
         io.sockets.emit('admin.message', {message:socket.name + " has left.", from:"admin"});
+        client.hdel("global:connectedUsers", socket.name);
     });
 });
 
@@ -109,6 +84,18 @@ io.sockets.on('connection', function(socket) {
 client.on("error", function(err) {
     console.log("ERR REDIS: " + err);
 });
+
+client.once("ready", function(err) {
+    console.log("Connected to redis.");
+    
+    client.hgetall("global:connectedUsers", function(err, res) {
+        for(key in res) {
+            client.hdel("global:connectedUsers", key);
+        }
+    });
+    
+    console.log("Done cleaning up connected users list.");
+})
 
 // We're going to need a list that's got recent chat messages in it so we
 // can run analytics on it. Shouts are going to need to be a separate list
