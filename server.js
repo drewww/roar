cf1ed0a06ce954e30dd79e5bd995ed23317e2562
Drew Harry <drew.harry@gmail.com>
2011-09-20 14:19:16 -0400

    Got a way more simple version of user auth ping/pong working. Solves the real problem - remembering people's names.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = require('express').createServer(),
     redis = require('redis'),
     client = redis.createClient(),
     crypto = require('crypto');
+    // process = require('process');
         
 app.listen(8080);
 
@@ -27,43 +28,34 @@ io.sockets.on('connection', function(socket) {
     // socket.emit('admin.message', {message: io.sockets.length + " people chatting."});
     
     socket.on('identify', function(data) {
-        // If it's got an auth section, check it. If not, generate a new 
-        // auth string and send it back.
-        if("auth" in data && "id" in data) {
-            // Check auth. Using the uid, see if it matches the auth key 
-            // for that user.
-            client.hgetall("users:" + data["id"], function (err, res) {
-                if(res["auth"] == data["auth"]) {
-                    // Good to go!
-                    socket.emit("identify", {username:res["username"]});
-                } else {
-                    socket.emit("identify", {username:null});
+        
+        
+        // eventually check this against redis to see if the name is taken
+        
+        if(true) { 
+            socket.emit("identify", {state:"OK", username:data["username"]});
+            
+            socket.name = data.username;    
+            
+            socket.broadcast.emit('admin.message', {message: socket.name + " has entered.", from:"admin"});
+            
+            client.lrange("room.messages", -10, -1, function (err, res) {
+                console.log("lrange returned");
+                console.log(res);
+                for(msgIndex in res) {
+                    console.log(res[msgIndex]);
+                    msgObj = JSON.parse(res[msgIndex]);
+                    msgObj["past"] = true;
+                    socket.emit('chat.message', msgObj);
                 }
                 
+                // Doing it here ensures that it appears after the past messages.
+                socket.emit('admin.message', {message: "You have joined the chat.", from:"admin"});
             });
             
             
-            
-        } else if("username" in data) {
-            // At this point, the user is requesting this name. Generate
-            // an auth string and send it down. 
-            // This is really awful security practice, but doesn't matter
-            // for this demo. 
-            hash = crypto.createHash('sha1');
-            hash.update(data["username"]);
-            hash.update('' + Date.now());
-            authString = hash.digest();
-            
-            // Now push this all into the database.
-            client.incr("global:nextUserId", function (err, res) {
-                
-                client.hset("users:" + res, "auth", authString);
-                client.hset("users:" + res, "username", data["username"]);
-                
-                socket.emit("identify", {"username":data["username"], "userId":res, "auth":authString});
-            });
         } else {
-            console.log("Malformed 'identify' message. Missing username and auth.");
+            socket.emit("identify", {state:"TAKEN"})
         }
         
     })
@@ -79,40 +71,44 @@ io.sockets.on('connection', function(socket) {
         client.ltrim("room.messages", 0, 100);
     });
     
-    socket.on('chat.identity', function(data) {
-        socket.name = data.name;
-
-        socket.broadcast.emit('admin.message', {message: socket.name + " has entered.", from:"admin"});
-        
-        client.lrange("room.messages", -10, -1, function (err, res) {
-            console.log("lrange returned");
-            console.log(res);
-            for(msgIndex in res) {
-                console.log(res[msgIndex]);
-                msgObj = JSON.parse(res[msgIndex]);
-                msgObj["past"] = true;
-                socket.emit('chat.message', msgObj);
-            }
-            
-            // Doing it here ensures that it appears after the past messages.
-            socket.emit('admin.message', {message: "You have joined the chat.", from:"admin"});
-        });
-        
-        
-        // Send the last 10 messages.
-    });
+    // socket.on('chat.identity', function(data) {
+    //     
+    //     // this will check to see if it's free or not, eventually)
+    //     if(true) 
+    //     
+    //     
+    //     
+    //     socket.name = data.name;
+    // 
+    //     socket.broadcast.emit('admin.message', {message: socket.name + " has entered.", from:"admin"});
+    //     
+    //     client.lrange("room.messages", -10, -1, function (err, res) {
+    //         console.log("lrange returned");
+    //         console.log(res);
+    //         for(msgIndex in res) {
+    //             console.log(res[msgIndex]);
+    //             msgObj = JSON.parse(res[msgIndex]);
+    //             msgObj["past"] = true;
+    //             socket.emit('chat.message', msgObj);
+    //         }
+    //         
+    //         // Doing it here ensures that it appears after the past messages.
+    //         socket.emit('admin.message', {message: "You have joined the chat.", from:"admin"});
+    //     });
+    //     
+    //     
+    //     // Send the last 10 messages.
+    // });
     
     socket.on('disconnect', function() {
         io.sockets.emit('admin.message', {message:socket.name + " has left.", from:"admin"});
     });
 });
 
-
 // Redis setup.
 client.on("error", function(err) {
     console.log("ERR REDIS: " + err);
-})
-
+});
 
 // We're going to need a list that's got recent chat messages in it so we
 // can run analytics on it. Shouts are going to need to be a separate list
