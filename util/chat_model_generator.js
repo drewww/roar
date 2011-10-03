efd711fb7840f563077db09d6a09926752fff83a
Drew Harry <drew.harry@gmail.com>
2011-10-03 17:30:46 -0400

    Language model generation works pretty well now.

--- util/chat_model_generator.js
+++ util/chat_model_generator.js
@@ -3,7 +3,8 @@
 
 var libxml = require("libxmljs"),
     logger = require('util'),
-    fs = require('fs');
+    fs = require('fs'),
+    sets = require('simplesets');
 
 logger.log("Starting chat model generator!");
 
@@ -14,3 +15,47 @@ logger.log("Loading file: " + filename);
 var chatlogXml = fs.readFileSync(filename, 'utf-8');
 var logDoc = libxml.parseXmlString(chatlogXml);
 
+var namesSet = new sets.Set([]);
+var nameNodes = logDoc.find("//envelope/sender");
+for(var nameNodeIndex in nameNodes) {
+    var nameNode = nameNodes[nameNodeIndex];
+    
+    namesSet.add(nameNode.text());
+}
+
+logger.log("names set: ");
+console.log("names set: ", namesSet.array());
+
+var messageNodes = logDoc.find("//envelope/message");
+var model = {};
+for(var messageNodeIndex in messageNodes) {
+    var messageNode = messageNodes[messageNodeIndex];
+    
+    // if you put punctuation in here, it'll turn out unpunctuated sentences.
+    // if you leave it in, it generates more specific results but probably
+    // limits its diversity of production. try it both ways.
+    var words = messageNode.text().split(/[\s]+/);
+    
+    // assuming 2-grams for now.
+    for(var i=0; i<words.length-1; i++) {
+        // loop through all the words with a two word window.
+        var curWords = words[i] + " " + words[i+1];
+        
+        var subsequentWords = {}
+        if(curWords in model) {
+            var subsequentWords = model[curWords];
+        }
+        
+        var score = 0;
+        if(words[i+2] in subsequentWords) {
+            score = subsequentWords[words[i+2]];
+        }
+        score++;
+        subsequentWords[words[i+2]] = score;
+        model[curWords] = subsequentWords;
+    }
+}
+
+console.log(model);
+
+// now normalize the model.
